ff97b51b4e3aaa19aa45959d3ed21c8ed11fda4f
loaisa <loaisa.zhenya@gmail.com>
2025-04-27 03:10:21 +0300

    liitle fix

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 import { CreatePostData } from '../types/types';
 
-// const API_URL = process.env.REACT_APP_API_URL || 'https://friendsposts.up.railway.app';
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:3001';
+const API_URL = process.env.REACT_APP_API_URL || 'https://friendsposts.up.railway.app';
+// const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:3001';
 const api = axios.create({
     baseURL: API_URL,
 });
